16d22d311b65b08aed6c4da2315996719f3c2340
Melissa Paisley <mpaisley@atlassian.com>
2025-05-17 20:15:05 +0100

    first commit

---
+++ src/frontend/data.jsx
@@ -0,0 +1,73 @@
+export const holidays = [
+  {
+    id: 1,
+    date: "2025-01-01",
+    title: "New Years Day",
+    description: "Our team will be out on vacation for New Year's Day. Our regular SLA will resume on January 2nd.", 
+  },
+  {
+    id: 2,
+    date: "2025-05-22",
+    title: "Memorial Day",
+    description: "Our team will be out on vacation for Memorial Day. Our regular SLA will resume on May 27th.",   },
+  {
+    id: 3,
+    date: "2025-07-04",
+    title: "Independence Day",
+    description: "Our team will be out on vacation for Independence Day. Our regular SLA will resume on July 5th.",   
+  },
+  {
+    id: 4,
+    date: "2025-09-01",
+    title: "Labor Day",
+    description: "Our team will be out on vacation for Labor Day. Our regular SLA will resume on September 2nd.",   
+  },
+  {
+    id: 5,
+    date: "2025-11-27",
+    title: "Thanksgiving Day",
+    description: "Our team will be out on vacation for Thanksgiving. Our regular SLA will resume on Friday 28th.",   
+  },    
+  {
+    id: 6,
+    date: "2025-12-25",
+    title: "Christmas Day",
+    description: "Our team will be out on vacation for Christmas Day. Our regular SLA will resume on December 26th.",   
+  },  
+  {
+    id: 7,
+    date: "2025-01-20",
+    title: "Martin Luther King Jr. Day",
+    description: "Our team will be out on vacation for Martin Luther King Jr. Day. Our regular SLA will resume on January 21st.",   
+  },
+  {
+    id: 8,
+    date: "2025-02-17",
+    title: "Washingtons Birthday",
+    description: "Our team will be out on vacation for Washingtons Birthday. Our regular SLA will resume on February 18th.",   
+  },
+  {
+    id: 9,
+    date: "2025-06-19",
+    title: "Juneteenth",
+    description: "Our team will be out on vacation for Juneteenth. Our regular SLA will resume on June 20th.",   
+  },
+  {
+    id: 10,
+    date: "2025-10-13",
+    title: "Columbus Day",
+    description: "Our team will be out on vacation for Columbus Day. Our regular SLA will resume on October 14th.",   
+  },
+  {
+    id: 11,
+    date: "2025-11-11",
+    title: "Veterans Day",
+    description: "Our team will be out on vacation for Veterans Day. Our regular SLA will resume on November 12th.",   
+  },
+  {
+    id: 12,
+    date: "2025-05-20",
+    title: "Spring bank holiday",
+    description: "Our UK team will be out on vacation for the spring bank holiday. Our regular SLA will resume on May 27th.",   
+  },
+];
---
+++ src/frontend/index.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import ForgeReconciler, { Text, useProductContext } from '@forge/react';
+import { holidays } from './data';
+
+const checkUpcomingHolidays = () => {
+    let upcomingHolidaysTemp = [];
+
+    /* Use the JavaScript Date object to get the current date and store as 'today'. */
+    let t = Date.now();
+    let today = new Date(t);
+    /* Use the JavaScript Date object to get the date 7 days from now and store it as 'nextWeek'. */
+    let n = Date.parse(new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7));
+    let nextWeek = new Date(n);
+
+    for (let index = 0; index < holidays.length; index++) {
+      /* Use the Javascript Date object to convert the date in our holidays object to a Date so it can be compared to the current date and next week. */
+      let holidayDate = new Date(holidays[index].date);
+      
+      if((holidayDate > today)&&(holidayDate < nextWeek) )
+      /* if the holiday date is after today, and less than a week away, add it to the array of upcoming holidays */
+      {
+          upcomingHolidaysTemp.push(holidays[index]);
+      }
+    }
+    return(upcomingHolidaysTemp);
+}
+
+const App = () => {
+  let upcomingHolidays = checkUpcomingHolidays();
+
+  return (
+    <>
+      {upcomingHolidays.map((holiday) => (
+        <>
+          <Text>{holiday.title}</Text>  
+          <Text>{holiday.description}</Text>
+        </>
+      ))}
+    </>
+  );
+};
+
+ForgeReconciler.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
